0fca1ccdb6505a4c6c7106cfa33a715b59c2bb06
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-04 17:46:10 +0300

    feat: add BrowserRouter and Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+
+import { store } from "./features/store.ts";
+
 import App from "./App.tsx";
+
+import "./styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>
 );
