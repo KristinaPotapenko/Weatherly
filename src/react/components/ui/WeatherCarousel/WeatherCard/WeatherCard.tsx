76f765ef8ebff7cbf9462a6095cd776a2a08a86c
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-04 22:21:36 +0300

    feat: create WeatherCard component

--- src/react/components/ui/WeatherCarousel/WeatherCard/WeatherCard.tsx
+++ src/react/components/ui/WeatherCarousel/WeatherCard/WeatherCard.tsx
@@ -5,9 +5,11 @@ import styles from "./WeatherCard.module.scss";
 
 interface WeatherProps {
   time: string;
-  image: string;
-  rainfall?: string;
-  temperature: string;
+  icon: string;
+  rainfall?: number;
+  temperature?: number;
+  temperatureMin?: number;
+  temperatureMax?: number;
   isCurrent: boolean;
 }
 
@@ -19,12 +21,36 @@ export const WeatherCard = ({
     <li className={cl(styles.card, { [styles.current]: isCurrent })}>
       <p className={styles.info}>{props.time}</p>
       <div className={styles.wrapper}>
-        <img className={styles.image} src={props.image} alt="Weather" />
+        <img
+          className={styles.icon}
+          src={`src/assets/${props.icon}.svg`}
+          alt="Weather"
+          onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+            e.currentTarget.onerror = null;
+            e.currentTarget.src = "src/assets/w_2.svg";
+          }}
+        />
         {props.rainfall && (
-          <p className={`${styles.info} ${styles.accent}`}>{props.rainfall}</p>
+          <p className={`${styles.info} ${styles.accent}`}>{props.rainfall}%</p>
         )}
       </div>
-      <p className={styles.infoTemperature}>{props.temperature}</p>
+      <div className={styles.temperature}>
+        {props.temperature && (
+          <p className={styles.infoTemperature}>
+            {props.temperature.toFixed(0)}°
+          </p>
+        )}
+        {props.temperatureMin && (
+          <p className={styles.infoTemperature}>
+            {props.temperatureMin.toFixed(0)}°
+          </p>
+        )}
+        {props.temperatureMax && (
+          <p className={styles.infoTemperature}>
+            {props.temperatureMax.toFixed(0)}°
+          </p>
+        )}
+      </div>
     </li>
   );
 };
