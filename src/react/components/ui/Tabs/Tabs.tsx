cf177e8fb9d2444aa344175d40a6a800680f4f43
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-07 22:13:00 +0300

    feat: add animation for Tabs

--- src/react/components/ui/Tabs/Tabs.tsx
+++ src/react/components/ui/Tabs/Tabs.tsx
@@ -1,5 +1,6 @@
 import { type JSX } from "react";
 import cl from "classnames";
+import { motion } from "framer-motion";
 
 import styles from "./Tabs.module.scss";
 
@@ -12,20 +13,29 @@ interface TabsProps {
 export const Tabs = ({ tabs, activeTab, onChange }: TabsProps): JSX.Element => {
   return (
     <div
-      className={`${cl(styles.tabs, {
+      className={cl(styles.tabs, {
         [styles.activeSecondTab]: activeTab === 1,
-      })}`}
+      })}
     >
       {tabs.map((tab, index) => {
+        const isActive = activeTab === index;
+
         return (
           <button
             key={index}
             className={` ${cl(styles.button, {
-              [styles.buttonActive]: activeTab === index,
+              [styles.buttonActive]: isActive,
             })}`}
             onClick={() => onChange(index)}
           >
             {tab}
+            {isActive && (
+              <motion.div
+                layoutId="underline"
+                className={styles.underline}
+                transition={{ type: "spring", stiffness: 500, damping: 30 }}
+              />
+            )}
           </button>
         );
       })}
