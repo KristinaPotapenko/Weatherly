72eb4c9acd97e1dd9ddc803f9d0f55428c28ce65
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-07 21:58:50 +0300

    feat: integration Loader component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import type { JSX } from "react";
 import { AppRoutes } from "./routes/Routes/AppRoutes";
+import { Loader } from "./react/popups/Loader/Loader";
+import { useSelector } from "react-redux";
+import { selectLoading } from "./features/appStatusSlice";
 
 function App(): JSX.Element {
+  const isLoading = useSelector(selectLoading);
+
   return (
-    <main>
-      <div>
-        <AppRoutes />
-      </div>
-    </main>
+    <>
+      {isLoading && <Loader />}
+      <main>
+        <div>
+          <AppRoutes />
+        </div>
+      </main>
+    </>
   );
 }
 
