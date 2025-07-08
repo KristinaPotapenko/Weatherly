2fe9cefffd719a4526e15f2f221830d4808eae59
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-08 22:24:52 +0300

    feat: add search route

--- src/routes/Routes/AppRoutes.tsx
+++ src/routes/Routes/AppRoutes.tsx
@@ -1,12 +1,16 @@
 import type { JSX } from "react";
 import { Route, Routes } from "react-router-dom";
+
 import { ROUTES } from "../routes";
+
 import { Home } from "../../react/pages/Home/Home";
+import { Search } from "../../react/pages/Search/Search";
 
 export const AppRoutes = (): JSX.Element => {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.SEARCH} element={<Search />} />
     </Routes>
   );
 };
