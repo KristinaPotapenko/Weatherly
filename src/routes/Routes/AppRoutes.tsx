1aeabb17e199d0e9da1eb01b361eea7d6c37c985
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-11 22:14:33 +0300

    feat: add forecast route

--- src/routes/Routes/AppRoutes.tsx
+++ src/routes/Routes/AppRoutes.tsx
@@ -5,12 +5,14 @@ import { ROUTES } from "../routes";
 
 import { Home } from "../../react/pages/Home/Home";
 import { Search } from "../../react/pages/Search/Search";
+import { Forecast } from "../../react/pages/Forecast/Forecast";
 
 export const AppRoutes = (): JSX.Element => {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
       <Route path={ROUTES.SEARCH} element={<Search />} />
+      <Route path={ROUTES.FORECAST} element={<Forecast />} />
     </Routes>
   );
 };
